f02a617de01a803e1cec1ce45f0ecdc0bfdf92cb
Diana Novikova <bogdanova.4.diana@yandex.ru>
2024-07-31 13:51:51 +0300

    Собрала вебпак

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import '../pages/index.css'
+import { initialCards } from './cards.js';
+
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 
@@ -34,3 +37,4 @@ initialCards.forEach(cardData => {
 
 
 console.log('Hello, World!');
+
