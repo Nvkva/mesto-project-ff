a309175521ea2c341e816e6565df0e7c3bbadaa3
Diana Novikova <bogdanova.4.diana@yandex.ru>
2024-08-21 21:26:40 +0300

    вынесла логику валидации в отдельный файл

---
+++ src/components/validation.js
@@ -0,0 +1,89 @@
+export const enableValidation = (config) => {
+  const formList = Array.from(document.querySelectorAll(config.formSelector));
+
+  formList.forEach((formElement) => {
+    const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+    const buttonElement = formElement.querySelector(config.submitButtonSelector);
+
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', () => {
+        if (inputElement.validity.patternMismatch) {
+          inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+        } else {
+          inputElement.setCustomValidity("");
+        }
+
+        if (!inputElement.validity.valid) {
+          showInputError({
+            formElement,
+            inputElement,
+            inputErrorClass: config.inputErrorClass,
+            errorClass: config.errorClass,
+          });
+        } else {
+          hideInputError({
+            formElement,
+            inputElement,
+            inputErrorClass: config.inputErrorClass,
+            errorClass: config.errorClass,
+          });
+        }
+
+        toggleButtonState({
+          buttonElement,
+          inputList,
+          inactiveButtonClass: config.inactiveButtonClass,
+        })
+      });
+    });
+  });
+};
+
+export const clearValidationForForm = (formElement, config) => {
+  const buttonElement = formElement.querySelector(config.submitButtonSelector);
+  buttonElement.disabled = true;
+  buttonElement.classList.add(config.inactiveButtonClass);
+
+  const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+  inputList.forEach((inputElement) => {
+    hideInputError({
+      formElement,
+      inputElement,
+      inputErrorClass: config.inputErrorClass,
+      errorClass: config.errorClass,
+    });
+  });
+};
+
+const showInputError = (inputErrorConfig) => {
+  const errorElement = inputErrorConfig.formElement.querySelector(`.${inputErrorConfig.inputElement.id}-error`);
+  inputErrorConfig.inputElement.classList.add(inputErrorConfig.inputErrorClass);
+  // Заменим содержимое span с ошибкой на переданный параметр
+  errorElement.textContent = inputErrorConfig.inputElement.validationMessage;
+  // Показываем сообщение об ошибке
+  errorElement.classList.add(inputErrorConfig.errorClass);
+};
+
+const hideInputError = (inputErrorConfig) => {
+  const errorElement = inputErrorConfig.formElement.querySelector(`.${inputErrorConfig.inputElement.id}-error`);
+  inputErrorConfig.inputElement.classList.remove(inputErrorConfig.inputErrorClass);
+  errorElement.classList.remove(inputErrorConfig.errorClass);
+  errorElement.textContent = '';
+};
+
+const toggleButtonState = (buttonStateConfig) => {
+  // Если есть хотя бы один невалидный инпут
+  if (hasInvalidInput(buttonStateConfig.inputList)) {
+    // сделай кнопку неактивной
+    buttonStateConfig.buttonElement.disabled = true;
+    buttonStateConfig.buttonElement.classList.add(buttonStateConfig.inactiveButtonClass);
+  } else {
+    // иначе сделай кнопку активной
+    buttonStateConfig.buttonElement.disabled = false;
+    buttonStateConfig.buttonElement.classList.remove(buttonStateConfig.inactiveButtonClass);
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => !inputElement.validity.valid);
+};
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './pages/index.css'
 import { initialCards } from './cards.js';
 import { createCard, deleteCard, likeCard } from './components/card.js';
 import { closeDialog, openDialog, closeDialogByOverlay } from './components/modal.js';
+import { clearValidationForForm, enableValidation } from './components/validation.js';
 
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
@@ -85,104 +86,6 @@ function handleCreation(evt) {
   closeDialog(createCardDialog);
 }
 
-const enableValidation = (config) => {
-  const formList = Array.from(document.querySelectorAll(config.formSelector));
-
-  formList.forEach((formElement) => {
-    const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
-    const buttonElement = formElement.querySelector(config.submitButtonSelector);
-
-    inputList.forEach((inputElement) => {
-      inputElement.addEventListener('input', () => {
-        if (inputElement.validity.patternMismatch) {
-          inputElement.setCustomValidity(inputElement.dataset.errorMessage);
-        } else {
-          inputElement.setCustomValidity("");
-        }
-
-        if (!inputElement.validity.valid) {
-          showInputError({
-            formElement,
-            inputElement,
-            inputErrorClass: config.inputErrorClass,
-            errorClass: config.errorClass,
-          });
-        } else {
-          hideInputError({
-            formElement,
-            inputElement,
-            inputErrorClass: config.inputErrorClass,
-            errorClass: config.errorClass,
-          });
-        }
-
-        toggleButtonState({
-          buttonElement,
-          inputList,
-          inactiveButtonClass: validationConfig.inactiveButtonClass,
-        })
-      });
-    });
-  });
-};
-
-const showInputError = (inputErrorConfig) => {
-  const errorElement = inputErrorConfig.formElement.querySelector(`.${inputErrorConfig.inputElement.id}-error`);
-  inputErrorConfig.inputElement.classList.add(inputErrorConfig.inputErrorClass);
-  // Заменим содержимое span с ошибкой на переданный параметр
-  errorElement.textContent = inputErrorConfig.inputElement.validationMessage;
-  // Показываем сообщение об ошибке
-  errorElement.classList.add(inputErrorConfig.errorClass);
-};
-
-const hideInputError = (inputErrorConfig) => {
-  const errorElement = inputErrorConfig.formElement.querySelector(`.${inputErrorConfig.inputElement.id}-error`);
-  inputErrorConfig.inputElement.classList.remove(inputErrorConfig.inputErrorClass);
-  errorElement.classList.remove(inputErrorConfig.errorClass);
-  errorElement.textContent = '';
-};
-
-const toggleButtonState = (buttonStateConfig) => {
-  // Если есть хотя бы один невалидный инпут
-  if (hasInvalidInput(buttonStateConfig.inputList)) {
-    // сделай кнопку неактивной
-    buttonStateConfig.buttonElement.disabled = true;
-    buttonStateConfig.buttonElement.classList.add(buttonStateConfig.inactiveButtonClass);
-  } else {
-    // иначе сделай кнопку активной
-    buttonStateConfig.buttonElement.disabled = false;
-    buttonStateConfig.buttonElement.classList.remove(buttonStateConfig.inactiveButtonClass);
-  }
-};
-
-const hasInvalidInput = (inputList) => {
-  // проходим по этому массиву методом some
-  return inputList.some((inputElement) => {
-    // Если поле не валидно, колбэк вернёт true
-    // Обход массива прекратится и вся функция
-    // hasInvalidInput вернёт true
-
-    return !inputElement.validity.valid;
-  })
-};
-
-const clearValidationForForm = (formElement, config) => {
-  const buttonElement = formElement.querySelector(config.submitButtonSelector);
-  buttonElement.disabled = true;
-  buttonElement.classList.add(config.inactiveButtonClass);
-
-  const inputList = Array.from(formElement.querySelectorAll(validationConfig.inputSelector));
-  inputList.forEach((inputElement) => {
-    hideInputError({
-      formElement,
-      inputElement,
-      inputErrorClass: validationConfig.inputErrorClass,
-      errorClass: validationConfig.errorClass,
-    });
-  });
-};
-
-
 // @todo: Вывести карточки на страницу
 initialCards.forEach(cardData => {
   const cardElement = createCard({ cardTemplate, cardData, deleteCard, likeCard, previewImage });
@@ -195,7 +98,6 @@ for (let item of popups) {
 
 imageCloseButton.addEventListener("click", () => closeDialog(imageDialog)); // Удаляем класс по нажатию на крестика
 imageDialog.addEventListener("click", (event) => closeDialogByOverlay(event, imageDialog)); // Удаляем класс по нажатию оверлей
-
 
 // Событие нажатия на кнопку открытия модального окна редактирования
 editButton.addEventListener("click", function () {
